15a9d8759c9eff750a595d2ed637df46ff60eac9
Lars Wirzenius <liw@liw.fi>
2013-04-18 09:14:34 +0100

    Add license summaries, remove project.meta

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2011  Lars Wirzenius <liw@liw.fi>
+# Copyright (C) 2011-2013  Lars Wirzenius <liw@liw.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# =*= License: GPL-3+ =*=
 
 from distutils.core import setup, Extension
 
